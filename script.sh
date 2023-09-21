df47842067ea109e2bfdfbf1631b636acc079d2f
Denis Iasenev <sfgronk@gmail.com>
2023-09-20 22:02:12 -0400

    add script

---
+++ script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+response_code=$(curl -s -o /dev/null -w '%{response_code}' ya.ru)
+echo "RESPONSE CODE: $response_code"
+
+if [ "$response_code" -ne "200" ]; then
+  echo "Server is unavailable"
+  exit 1
+fi
+
+echo "Server available and returns 200 status code"
+exit 0
